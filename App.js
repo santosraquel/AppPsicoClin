1c529f1110bea6b17ea564b5f6407f2f021d9248
santosraquel <raquel.dossantos1965@gmail.com>
2022-01-27 12:00:08 -0300

    creating project and testing commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigation } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Consultation">
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
